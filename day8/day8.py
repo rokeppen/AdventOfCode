920ab37a113e910c1051a080630ba5ee6244d1dd
rokeppen <robinkep@gmail.com>
2021-12-08 22:06:04 +0100

    Day 8, but better

--- day8/day8.py
+++ day8/day8.py
@@ -5,19 +5,19 @@ def get_simple_count(file):
     return sum(1 for line in file for x in line.split('|')[1].strip().split(' ') if 1 < len(x) < 5 or len(x) == 7)
 
 
-def decode(string, key):
-    return string.translate(string.maketrans(''.join(key), "abcdefg"))
+def decode(x, key):
+    return ''.join(sorted(x.translate(x.maketrans(''.join(key), "abcdefg"))))
 
 
 def get_count(file):
-    result = 0
     table = {"abcdef": 0, "bc": 1, "abdeg": 2, "abcdg": 3, "bcfg": 4, "acdfg": 5, "acdefg": 6, "abc": 7, "abcdefg": 8, "abcdfg": 9}
+    result, keys = 0, sorted(list(table.keys()))
     for line in file:
         numbers, output = line.split('|')[0].strip().split(' '), line.split('|')[1].strip().split(' ')
         for per in permutations("abcdefg"):
-            translation = [''.join(sorted(decode(n, per))) for n in numbers]
-            if sorted(translation) == sorted(list(table.keys())):
-                result += int(''.join(str(table.get(''.join(sorted(decode(n, per))))) for n in output))
+            translation = [decode(n, per) for n in numbers]
+            if sorted(translation) == keys:
+                result += int(''.join(str(table.get(decode(n, per))) for n in output))
                 break
     return result
 
